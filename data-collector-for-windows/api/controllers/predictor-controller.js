8546a4e2abfacd8729d5a343e4c033b7279e19d9
BambinoYehNCKU <bambinoyeh@imrc.ncku.edu.tw>
2019-01-30 11:43:12 +0800

    姿雁大聯盟

---
+++ data-collector-for-windows/api/controllers/predictor-controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const child_process = require('child_process');
+var config = require('../../config.json');
+
+exports.predict_result= function(req, res, callback) {
+	var predictItemId='';
+	if ( typeof req.params.predictItemId !== 'undefined' && req.params.predictItemId )
+		predictItemId=req.params.predictItemId;
+
+	try {
+
+		var exe_path = config.algorithm_path;
+		var raw_data_path = config.raw_data_path;
+		var algorithm_cmd = config.algorithm_cmd;
+		console.log(algorithm_cmd+' '+raw_data_path+' '+predictItemId);
+		var workerProcess = child_process.exec(algorithm_cmd+' '+raw_data_path+' '+predictItemId, {cwd: exe_path},
+			function (error, stdout, stderr) {
+				if (error) {
+					console.log(error.stack);
+					console.log('Error code: '+error.code);
+					console.log('Signal received: '+error.signal);
+				}
+				console.log(`stdout: ${stdout}`);
+				//return "{"+stdout+",result:success}";
+				var split_char="&";
+				var result = stdout.split(split_char);
+				callback(result);
+				
+
+			}
+		);
+		workerProcess.on('exit', function (code) {
+			console.log('Child process exited with exit code ' + code);
+		});
+	} catch (err) {
+	  callback(err);
+	}
+		
+};
+
